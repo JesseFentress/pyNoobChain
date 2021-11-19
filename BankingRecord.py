3b43fdefab0ca110824f85cc60c80f77bffdd125
Joe Oakes <joe.oakes@gmail.com>
2021-11-18 20:28:26 -0500

    intial

--- BankingRecord.py
+++ BankingRecord.py
@@ -1,30 +1,29 @@
 
-class BankingRecord
-  def __init__(self, accountNumber, accountType, transactionType, transactionAmount, balance)
-    self.accountNumber = accountNumber
-	self.accountType = accountType
-	self.transactionType = transactionType
-	self.transactionAmount = transactionAmount
-	self.balance = balance
+class BankingRecord:
+  def __init__(self, accountNumber, accountType, transactionType, transactionAmount, balance):
+   self.accountNumber = accountNumber
+   self.accountType = accountType
+   self.transactionType = transactionType
+   self.transactionAmount = transactionAmount
+   self.balance = balance
 
-    //Now start setters and getters
-  def getAccountNumber(self)
+  def getAccountNumber(self):
     return self.accountNumber
-  def setAccountNumber(self, _ActN)
+  def setAccountNumber(self, _ActN):
     self.accountNumber = _ActN
-  def getAccountType(self) 
+  def getAccountType(self):
     return self.accountType
-  def setAccountType(self, _ActT)
+  def setAccountType(self, _ActT):
    self.accountType = _ActT
-  def getTransactionType(self) 
+  def getTransactionType(self):
    return self.transactionType
-  def setTransactionType(self, _TraT)
-	self.transactionType = _TraT
-  def getTransactionAmount(self)
+  def setTransactionType(self, _TraT):
+   self.transactionType = _TraT
+  def getTransactionAmount(self):
    return self.transactionAmount
-  def setTransactionAmount(self, _TraA)
-	self.transactionAmount = _TraA
-  def getBalance(self)
+  def setTransactionAmount(self, _TraA):
+   self.transactionAmount = _TraA
+  def getBalance(self):
    return self.balance
-  def setBalance(self, _Bal)
-	self.balance = _Bal
+  def setBalance(self, _Bal):
+   self.balance = _Bal
