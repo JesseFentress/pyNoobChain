0e2041d9e020c9f00d5428b993bcf4a363f1fdae
Joe Oakes <joe.oakes@gmail.com>
2021-11-18 20:34:53 -0500

    intial

--- Block.py
+++ Block.py
@@ -1,8 +1,8 @@
 
 import hashlib
 
-class Block
-  def __init__(self, index,hash, previousHash, data, timeStamp, nonce)
+class Block:
+  def __init__(self, index,hash, previousHash, data, timeStamp, nonce):
    self.index = index
    self.hash = hash
    self.previousHash = previousHash
@@ -10,17 +10,17 @@ class Block
    self.timeStamp = timeStamp
    self.nonce = nonce
    
-  def calculateHash(self)
+  def calculateHash(self):
    block = self.previousHash + self.timeStamp + self.nonce + data
    calculatedhash = hashlib.sha256(block.encode())
    return calculatedhash   
 
-  def mineBlock(self, difficulty) 
-        target = StringUtil.getDificultyString(difficulty); 
-        while(!hash.substring( 0, difficulty).equals(target))
-            self.nonce++
-            self.hash = calculateHash();
-        print("Block Mined!!! : ", self.hash)
+  def mineBlock(self, difficulty): 
+   target = StringUtil.getDificultyString(difficulty); 
+   while(!hash.substring( 0, difficulty).equals(target)):
+     self.nonce++
+     self.hash = calculateHash();
+   print("Block Mined!!! : ", self.hash)
 
-  def getData(self)
-      return self.data
+  def getData(self):
+    return self.data
