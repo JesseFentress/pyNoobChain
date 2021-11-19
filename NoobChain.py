7db5d3da0cbe5007e892490a2706fc5c6c6787ec
Joe Oakes <joe.oakes@gmail.com>
2021-11-18 20:15:18 -0500

    intial

--- NoobChain.py
+++ NoobChain.py
@@ -1,7 +1,9 @@
+import BankingRecord
+import AES
 
 class NoobChain
   def __init__(self, blockchain, difficulty)
-    self.blockchain = blockchai;
+    self.blockchain = blockchain;
 	self.difficulty = difficulty
 
 #ArrayList<Block> blockchain = new ArrayList<Block>();
@@ -27,20 +29,16 @@ def __main__(self) {
   bankRecord2.setTransactionType("Deposit")
   bankRecord2.setTransactionAmount("1000")
   bankRecord2.setBalance("2000")
-  jsonBank = gson.toJson(bankRecord)
-  encryptedBankRecord = AES.encrypt(jsonBank, secretKey)
-  addBlock(new Block(encryptedMedicalRecord,blockchain.get(blockchain.size()-1).hash))
+  jsonBank = gson.toJson(bankRecord2)
+  encryptedBankRecord2 = AES.encrypt(jsonBank, secretKey)
+  addBlock(new Block(encryptedMedicalRecord2,blockchain.get(blockchain.size()-1).hash))
 
-  println("\nBlockchain is Valid: " + isChainValid());
+  print("\nBlockchain is Valid: " + isChainValid())
+  blockchainJson = StringUtil.getJson(blockchain)
+  print("\nThe block chain: ", blockchainJson);
+  for(int i = 0; i < blockchain.size(); i++):
+     print("\nDecrypted block data for block #" + (i+1) + ": " + AES.decrypt(blockchain.get(i).getData(), secretKey))
 
-  blockchainJson = StringUtil.getJson(blockchain);
-   print("\nThe block chain: ");
-   print(blockchainJson);
-
-        for(int i = 0; i < blockchain.size(); i++){
-            print("\nDecrypted block data for block #" + (i+1) + ": " + AES.decrypt(blockchain.get(i).getData(), secretKey))
-        }
-    }
 
     //method for checking the integriy of the blockchain
   def isChainValid(self)
